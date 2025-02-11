6807a06fd64c2f43785ac6efd0e2876ab2ba290e
Shivaraj S <shivarajskit@gmail.com>
2025-02-11 10:38:26 +0530

    node js - create server - render different content for each request url

--- create server/app.js
+++ create server/app.js
@@ -2,6 +2,19 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Hello World</title></head>');
+    if (req.url === '/') {
+        res.write('<body><h1>Home</h1></body>');
+    } else if (req.url === '/products') {
+        res.write('<body><h1>My Products</h1></body>');
+    } else {
+        res.write('<body><h1>First Node Test</h1></body>');
+    }
+    res.write('</html>');
+    res.end();
     //process.exit(); // to end event loop
 });
 
