eb3d86e81012d4152304eb3eeeb01df7a4cf6fdd
Shivaraj S <shivarajskit@gmail.com>
2025-02-13 10:50:05 +0530

    node js - create server with module export/import

--- create server/app.js
+++ create server/app.js
@@ -1,22 +1,7 @@
 const http = require('http');
+const userRequestHandler = require('./user');
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Hello World</title></head>');
-    if (req.url === '/') {
-        res.write('<body><h1>Home</h1></body>');
-    } else if (req.url === '/products') {
-        res.write('<body><h1>My Products</h1></body>');
-    } else {
-        res.write('<body><h1>First Node Test</h1></body>');
-    }
-    res.write('</html>');
-    res.end();
-    //process.exit(); // to end event loop
-});
+const server = http.createServer(userRequestHandler);
 
 const PORT = 3001
 server.listen(PORT, () => {
